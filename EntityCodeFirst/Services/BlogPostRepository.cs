3fde7f7dedb26eb2300ff2f8a6ee9f0f533f9f99
FREDCOM-PC <FREDCOM-PC@FREDCOM-PC>
2018-03-15 13:11:00 -0400

    Crear Controlador de BlogPosts y Crear vista para Crear nuevo

---
+++ EntityCodeFirst/Services/BlogPostRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EntityCodeFirst.Models;
+
+namespace EntityCodeFirst.Services
+{
+    public class BlogPostRepository
+    {
+        public List<BlogPost> ObtenerTodos()
+        {
+            using (var db = new BlogContext())
+            {
+                return db.BlogPosts.ToList();               
+            }
+        }
+
+        internal void Crear(BlogPost model)
+        {
+            using (var db = new BlogContext())
+            {
+                db.BlogPosts.Add(model);
+                db.SaveChanges();
+            }
+        }
+    }
+}
